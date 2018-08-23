dee8abcc4f47c46b266dcf54bab1d59636ec1dc4
Rachael Njeri <rachael.njeri@andela.com>
2018-08-23 21:38:27 +0300

    setup the header component
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- albums/index.js
+++ albums/index.js
@@ -1,7 +1,8 @@
-/** @format */
+import React from 'react';
+import { AppRegistry } from 'react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import Header from './src/components/header';
 
-AppRegistry.registerComponent(appName, () => App);
+const App = () => <Header />;
+
+AppRegistry.registerComponent('albums', () => App);
---
+++ albums/src/components/header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  },
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 15,
+    height: 60,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 0,
+    position: 'relative'
+  }
+};
+
+const Header = () => {
+  const { textStyle, viewStyle } = styles;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums</Text>
+    </View>
+  );
+};
+
+export default Header;
