c3c4282b9a3c2ebbfe46a483707f9b06eacfd173
Rachael Njeri <rachael.njeri@andela.com>
2018-08-24 12:12:34 +0300

    Re-usable Card component
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends React.Component {
   state = {
@@ -13,13 +15,15 @@ class AlbumList extends React.Component {
       .then(response => this.setState({ albums: response.data }));
   }
 
+  renderAlbums() {
+    return this.state.albums.map(album => (
+      <AlbumDetail key={album.title} album={album} />
+    ));
+  }
+
   render() {
     console.log(this.state);
-    return (
-      <View>
-        <Text>Album List</Text>
-      </View>
-    );
+    return <View>{this.renderAlbums()}</View>;
   }
 }
 
