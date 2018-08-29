26c6384dba5e078578d0599ac9c3111ebebff11b
Rachael Njeri <rachael.njeri@andela.com>
2018-08-29 17:39:10 +0300

    login error handling
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './commons';
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 class LoginForm extends Component {
   state = {
     email: '',
-    password: ''
+    password: '',
+    error: ''
   };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            this.setState({ error: 'Authentication Failed!' });
+          });
+      });
+  }
 
   render() {
     return (
@@ -29,8 +55,10 @@ class LoginForm extends Component {
           />
         </CardSection>
 
+        <Text style={styles.errorTextStyle}>{this.state.error}</Text>
+
         <CardSection>
-          <Button>Login</Button>
+          <Button onPress={this.onButtonPress.bind(this)}>Login</Button>
         </CardSection>
       </Card>
     );
