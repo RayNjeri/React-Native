4e3100214ef6059a6afe0d6d29bf4c2441779d71
Rachael Njeri <rachael.njeri@andela.com>
2018-08-29 11:38:21 +0300

    React Native linking
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const styles = {
   headerContentStyle: {
@@ -30,7 +31,7 @@ const styles = {
 };
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   const {
     thumbnailStyle,
     headerContentStyle,
@@ -54,6 +55,10 @@ const AlbumDetail = ({ album }) => {
 
       <CardSection>
         <Image style={imageStyle} source={{ uri: image }} />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)}>Buy Now</Button>
       </CardSection>
     </Card>
   );
