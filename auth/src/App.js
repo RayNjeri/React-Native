dcdbdf32dd8690fb09fe2954770ba7b5b133ebcf
Rachael Njeri <rachael.njeri@andela.com>
2018-08-30 09:20:40 +0300

    handle Log out
    Signed-off-by: Rachael Njeri <rachael.njeri@andela.com>

--- auth/src/App.js
+++ auth/src/App.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
 
-import { Header } from './components/commons';
+import { Header, Button, Spinner } from './components/commons';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = {
+    loggedIn: null
+  };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyCeB85hly053dMypMKcOzPqsoWFHp2qL2I',
@@ -15,13 +19,34 @@ class App extends Component {
       storageBucket: 'auth-c2699.appspot.com',
       messagingSenderId: '937691789690'
     });
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
