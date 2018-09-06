67b3a258a5d1c3ec2a4a120124d33d66912d9b8a
Rachael Njeri <rachael.njeri@andela.com>
2018-09-06 20:00:06 +0300

    Configure Actions

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
+
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 const styles = {
   titleStyle: {
@@ -11,6 +14,7 @@ const styles = {
 
 class ListItem extends Component {
   render() {
+    console.log(this.props, 'hhhhhh');
     const { titleStyle } = styles;
     return (
       <CardSection>
@@ -20,4 +24,7 @@ class ListItem extends Component {
   }
 }
 
-export default ListItem;
+export default connect(
+  null,
+  actions
+)(ListItem);
